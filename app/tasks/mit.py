7748a5514b3a47c3bb311dcbbca4b584b44960b5
Wang Guan <momocraft@gmail.com>
2024-03-12 03:56:44 +0900

    wip

---
+++ app/tasks/mit.py
@@ -0,0 +1,10 @@
+"""
+Text segmentation + OCR using manga-image-translator
+"""
+
+from app import celery as celery_app
+
+
+@celery_app.task(name="tasks.mit_detection")
+def mit_detection(path_or_url: str, **kwargs):
+    pass # Real implementation is in manga_translator/moeflow_worker.py
